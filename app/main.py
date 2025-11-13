28110f5965d6ca8f3d8f9ff46306cfffe65d3643
Siddartha57 <112617980+Siddartha57@users.noreply.github.com>
2025-11-14 00:41:10 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -2,15 +2,6 @@ from fastapi import FastAPI
 from .database import Base, engine
 from .scheduler import start_scheduler
 from .api.routes import router
-from fastapi.middleware.cors import CORSMiddleware
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],  # or specific domain
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 
 Base.metadata.create_all(bind=engine)
