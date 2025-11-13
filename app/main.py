bf953bdf67f98e4c4673de1c12d2272d2475d650
Siddartha57 <112617980+Siddartha57@users.noreply.github.com>
2025-11-14 00:49:43 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -2,11 +2,19 @@ from fastapi import FastAPI
 from .database import Base, engine
 from .scheduler import start_scheduler
 from .api.routes import router
-
+from fastapi.middleware.cors import CORSMiddleware
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Job Search API")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.on_event("startup")
 async def startup_event():
